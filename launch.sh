a29eec6673aad7acf5b6ad6b86ecdc4b2ad6a598
Ratomas <ratomas79@gmail.com>
2022-02-19 13:42:37 -0600

    Update launch.sh
    removed references for eula.txt in setup folder

--- launch.sh
+++ launch.sh
@@ -55,9 +55,7 @@ fi
 if [[ -n "$LEVELTYPE" ]]; then
     sed -i "/level-type\s*=/ c level-type=$LEVELTYPE" server.properties
 fi
-if [[ -n "$EULA" ]]; then
-    sed -i "/eula\s*=/ c eula=$EULA" setup/eula.txt
-fi
+
 if [[ -n "$OPS" ]]; then
     echo $OPS | awk -v RS=, '{print}' >> ops.txt
 fi
